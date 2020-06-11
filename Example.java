6869f7775827444e2244d95b7ee3e65af7c3fbf0
朱思楠 <zsn990205@163.com>
2020-06-12 07:30:19 +0800

    Example

---
+++ Example.java
@@ -0,0 +1,8 @@
+public class Example {
+
+
+        public static void main(String[] args){
+            System.out.println("A".compareTo("a"));
+            System.out.println("B".compareTo("d"));
+        }
+    }
