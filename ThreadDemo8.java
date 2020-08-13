843f2d76b236b5ca00a22737d4fba91f50925a8a
朱思楠 <zsn990205@163.com>
2020-08-13 13:28:18 +0800

    ThreadDemo8

---
+++ ThreadDemo8.java
@@ -0,0 +1,37 @@
+
+
+public class ThreadDemo8 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread() {
+            @Override
+            public void run() {
+          for (int i = 0; i < 10; i++) {
+              System.out.println("我是线程1");
+              try {
+                  Thread.sleep(100);
+              } catch (InterruptedException e) {
+                  e.printStackTrace();
+              }
+          }
+            }
+        };
+        Thread t2 = new Thread() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("我是线程2");
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        t1.start();
+        t1.join();
+        t2.start();
+        t2.join();
+        System.out.println("主线程执行完毕");
+    }
+}
