58cf1df88886d3632563519f66e57ca346d3b7bc
朱思楠 <zsn990205@163.com>
2020-10-11 09:51:26 +0800

    Demo2

---
+++ Demo2.java
@@ -0,0 +1,32 @@
+
+
+class AA {
+    public AA() {
+        System.out.println("AA.构造");
+    }
+    {
+        System.out.println("AA.{}");
+    }
+    static {
+        System.out.println("AA.static");
+    }
+}
+class BB extends AA {
+    public BB() {
+        System.out.println("BB.构造");
+    }
+    {
+        System.out.println("BB.{}");
+    }
+    static {
+        System.out.println("BB.static");
+    }
+}
+public class Demo2 extends BB {
+    public static void main(String[] args) {
+        System.out.println("开始");
+         new BB();
+         new BB();
+        System.out.println("结束");
+    }
+}
